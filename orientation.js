fa406ff29ce3ea9299b2f057c95de004c5cf8c18
madalinapena <44406575+madalinapena@users.noreply.github.com>
2018-10-30 19:48:07 +0200

    Update orientation.js

--- orientation.js
+++ orientation.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.10.30.2";
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.10.30.3";
 
 window.addEventListener("deviceorientation", la_schimbare_gyro);
 
@@ -12,6 +12,6 @@ function la_schimbare_gyro(e)
  var context = canvas.getContext("2d");
  
  context.beginPath();
- context.arc(canvas.width / 2, canvas.height / 2,30,0,2 *Math.PI
+ context.arc(canvas.width / 2, canvas.height / 2,30,0,2 *Math.PI);
              context.stroke();
  }            
