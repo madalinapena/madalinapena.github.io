39b4c54ff73c60d3fe3538dbbc6fc2cbe02e9306
madalinapena <44406575+madalinapena@users.noreply.github.com>
2018-10-30 18:38:45 +0200

    Update program.js

--- program.js
+++ program.js
@@ -3,11 +3,11 @@ function modifica()
 {
 document.getElementById("id_salut").innerHTML="Hello";
 if (pisica==false){
-document.getElementById("id_img").src="cat.jpg";
-pisica = true;
+    document.getElementById("id_img").src="cat.jpg";
+    pisica = true;
 }
 else{
-document.getelementById("id_img").src="cat.jpj";
-pisica=false;
+     document.getElementById("id_img").src="cat.jpg";
+     pisica=false;
 }
 }
