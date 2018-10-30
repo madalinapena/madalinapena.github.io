ab4cde7af197f4d119eb5bdaedda81c67fca718a
madalinapena <44406575+madalinapena@users.noreply.github.com>
2018-10-30 19:21:04 +0200

    Update orientation.js

--- orientation.js
+++ orientation.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML ="Logic version: 2018.10.30.0";
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.10.30.0";
 
 window.addEventListener("deviceorientation", la_schimbare_gyro);
 
