183fe94eb52b3acafd8ed30b5988ff6967d138e7
madalinapena <44406575+madalinapena@users.noreply.github.com>
2018-11-06 19:32:42 +0200

    Update orientation.js

--- orientation.js
+++ orientation.js
@@ -1,13 +1,25 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.11.06.6";
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.11.06.7";
 
 window.addEventListener("deviceorientation", la_schimbare_gyro);
 window.addEventListener("deviceorientation", la_schimbare_acc);
 
 function la_schimbare_acc(e)
 {
+ var acc_x = e.accelerationIncludingGravity.x;
+ var acc_y = e.accelerationIncludingGravity.y;
+ var acc_z = e.accelerationIncludingGravity.z;
+ 
+  document.getElementById("id_acc_x").innerHTML = "acc x="=acc_x;
+  document.getElementById("id_acc_x").innerHTML =  "acc y="=acc_y;
+  document.getElementById("id_acc_x").innerHTML =  "acc z="=acc_z;
+ 
+ document.getElementById("id_rot_x").innerHTML=Mach.atan(acc_x/acc_z)*180/Math.PI;
+  document.getElementById("id_rot_x").innerHTML=Mach.atan(acc_y/acc_z)*180/Math.PI;
+}
+ 
  document.getElementById("id_acc_x").innerHTML = e.accelerationIncludingGravity.x;
- document.getElementById("id_acc_x").innerHTML = e.accelerationIncludingGravity.y;
- document.getElementById("id_acc_x").innerHTML = e.accelerationIncludingGravity.z;
+ document.getElementById("id_acc_y").innerHTML = e.accelerationIncludingGravity.y;
+ document.getElementById("id_acc_z").innerHTML = e.accelerationIncludingGravity.z;
  
 }
 function la_schimbare_gyro(e)
