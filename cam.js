3b62c4fd14e151592374e2d41042fe2fb0de4ba1
madalinapena <44406575+madalinapena@users.noreply.github.com>
2018-11-13 19:28:20 +0200

    Add files via upload

--- cam.js
+++ cam.js
@@ -1,6 +1,6 @@
-document.getElementById("id_logic_version").innerHTML= "logic vertion:2018.11.13.4";
-document.getElementById("id_video"). addEventListener(mousedown",on_touch);
-document.getElementById("id_video"). addEventListener(touchstart",on_touch);
+document.getElementById("id_logic_version").innerHTML= "logic vertion:2018.11.13.5";
+document.getElementById("id_video"). addEventListener("mousedown",on_touch);
+document.getElementById("id_video"). addEventListener("touchstart",on_touch);
 
 function on_ok(stream)
 {
